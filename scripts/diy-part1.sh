fce2a66a91933e59fdc2522f7a07cc5aa416fc25
zhoufeng <zhf883680@gmail.com>
2023-07-17 16:08:07 +0800

    无ssrp版本

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
